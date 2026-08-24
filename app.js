f101c21daa9e71328b5a3e995866658f7e66e7cc
FaganFeyzili <feyzilifeqan@gmail.com>
2026-08-24 12:46:39 +0000

    commit

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
 "use strict";
 $(document).ready(function () {
 
+    $("#me").click(function (e){
+        e.preventDefault();
+
+        function myel(myclass = ""){
+            if($.trim($("#inputme").val()) !== ''){
+                let vall = $("#inputme").val()
+                let newH3 = $('<h3> </h3>')
+                let newDiv = $('<div class="line"></div>')
+                let bigDiv = $(`<div ${myclass}></div>`)
+
+                $(newH3).append(vall)
+                $(newDiv).append(newH3)
+                $(bigDiv).append(newDiv)
+                return bigDiv
+            }
+            else{
+                return
+            }
+        }
+
+        $("#messageme").append(myel('class="end"'))
+        $("#inputme").val("")
+    });
+
 });
